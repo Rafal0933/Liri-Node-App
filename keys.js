96bcf2121aad8852fc4042b0fd533a1116b87bee
Rafal Kociolek <Rafal0933@hotmail.com>
2017-11-18 01:30:07 -0600

    created files

---
+++ keys.js
@@ -0,0 +1,18 @@
+console.log('Keys is loaded');
+
+var twitterKeys = {
+  consumer_key: ' 6H0FdIhqJsfXrYd7CNWDNQ8RF',
+  consumer_secret: '43jxx4QNd6UDviGh97NjBvj6O5Vdy6dl2e8Htg22JDPBvnwM3I',
+  access_token_key: ' 929879959019118592-halx7p4vLsnWpJ7ibLA452qur0rfcV9',
+  access_token_secret: '8pizdz3i62gyvJF3viH1aUtibEwBkjKM3UIkAodGKVqYL',
+}
+
+var spotifyKeys = {
+    id: "5cf80447e0774589b138441fb40b634f",
+    secret: "a1c30e17f1c24c988524c12b87b26920"
+}
+
+module.exports = {
+    twitterKeys,
+    spotifyKeys
+}
---
+++ test2.js
@@ -0,0 +1,6 @@
+var request = require("request");
+
+// Store all of the arguments in an array
+var nodeArgs = process.argv;
+
+console.log(process.argv);
